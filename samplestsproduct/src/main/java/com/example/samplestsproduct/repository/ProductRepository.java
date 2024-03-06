d764cb109f18e8ead20c9aa6f9b2e0a6462705ca
Kishore <kishore2603>
2024-03-06 08:12:05 -0600

    Deleted the other folders

--- samplestsproduct/src/main/java/com/example/samplestsproduct/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.samplestsproduct.repository;
-
-import com.example.samplestsproduct.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
